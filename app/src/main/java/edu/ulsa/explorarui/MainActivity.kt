ac518540d5b3c6aaa6019a1df0a12fc4a1820f01
Emiliano <emilianolopezn@outlook.com>
2025-09-30 19:46:37 -0700

    Se integro el Components playground

--- app/src/main/java/edu/ulsa/explorarui/MainActivity.kt
+++ app/src/main/java/edu/ulsa/explorarui/MainActivity.kt
@@ -6,15 +6,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -29,6 +38,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import edu.ulsa.explorarui.ui.theme.ExplorarUITheme
 
 class MainActivity : ComponentActivity() {
@@ -49,9 +59,15 @@ fun AppRoot() {
     val snackbarHostState = remember { SnackbarHostState() }
     var contador by remember { mutableStateOf(0) }
 
+    LaunchedEffect(contador) {
+        if (contador > 0) {
+            snackbarHostState.showSnackbar("Clicks: $contador")
+        }
+    }
+
     Scaffold (
         topBar = {
-            TopAppBar(title = { Text("Explorar UI")})
+            TopAppBar(title = { Text("Explorarando UI")})
         },
         floatingActionButton = {
             FloatingActionButton(onClick = { contador++ }) {
@@ -63,9 +79,7 @@ fun AppRoot() {
         HomeScreen(
             modifier = Modifier.padding(innerPadding),
             onShowSnackBar = {
-                LaunchedEffect(contador) {
-                    snackbarHostState.showSnackbar("Clicks: $contador")
-                }
+                contador ++
             }
         )
 
@@ -74,7 +88,32 @@ fun AppRoot() {
 
 @Composable
 fun HomeScreen(modifier : Modifier = Modifier, onShowSnackBar:  () -> Unit) {
-    Box(modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Button(onClick = onShowSnackBar) { Text("Mostrar Snackbar")}
+    /*Box(modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Button(onClick = onShowSnackBar) { Text("Aumentar contador")}
+    }*/
+   // Spacer(Modifier.height(40.dp))
+    ComponentsPlayground()
+}
+
+@Composable
+fun ComponentsPlayground() {
+    var text by remember { mutableStateOf("Hola mundo") }
+    var enabled by remember { mutableStateOf(true) }
+    var sliderValue by remember { mutableStateOf(0.5f) }
+    var checked by remember { mutableStateOf(false) }
+    var colorToggle by remember { mutableStateOf(false) }
+
+    val animatedTint by animateColorAsState(
+        targetValue = if (colorToggle) MaterialTheme.colorScheme.tertiary
+        else MaterialTheme.colorScheme.primary,
+        label = "tint"
+    )
+
+    Column(Modifier.verticalScroll(rememberScrollState()).padding(16.dp), verticalArrangement = Arrangement.spacedBy(16.dp))   {
+        Text("Tipografías", style = MaterialTheme.typography.headlineSmall)
+        Text("BodyLarge", style = MaterialTheme.typography.bodyLarge)
+        Text("BodyLarge", style = MaterialTheme.typography.bodyLarge)
+
+
     }
 }
--- app/src/main/java/edu/ulsa/explorarui/ui/theme/Type.kt
+++ app/src/main/java/edu/ulsa/explorarui/ui/theme/Type.kt
@@ -10,10 +10,10 @@ import androidx.compose.ui.unit.sp
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 34.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = 0.8.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
